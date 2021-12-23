33fe77bc043926894e162a6239108d8c1b9b0209
neomog <ohiarenathaniel@gmail.com>
2021-12-23 03:44:32 +0100

    image correction

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/../public/images/profile2.jpeg"
+              src="/profile2.jpeg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
